16d13c90512eb3f0b6924c6223af59261f7c5dc7
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2018-11-06 20:08:13 -0600

    Update hm4_Xiaojie_Zhu.asm

--- hm4_Xiaojie_Zhu.asm
+++ hm4_Xiaojie_Zhu.asm
@@ -1,3 +1,42 @@
 	.data
-str_suffix:	.asciiz		"plain.txt"
-str_content:	.asciiz		"The quick brown fox jumped over the lazy river."
+fileName:	.asciiz		"homework4.txt"
+txtBuffer:	.asciiz		"The quick brown fox jumps over the lazy dog."
+inputBuffer:	.space		100
+anotherBuffer:	.space		100
+
+	.text
+open_file:	li	$v0,	13
+		la	$a0,	fileName
+		li	$a1,	1
+		li	$a2,	0
+		syscall
+		move	$s6,	$v0
+
+write_file:	li	$v0,	15
+		move	$a0,	$s6
+		la	$a1,	txtBuffer
+		li	$a2,	44
+		syscall
+
+exit_file:	li	$v0,	16
+		move	$a0,	$s6
+		syscall
+
+open_created:	li	$v0,	13
+		la	$a0,	fileName
+		li	$a1,	0
+		li	$a2,	0
+		syscall
+		move	$s6,	$v0
+		
+read_file:	li	$v0,	14
+		move	$a0,	$s6
+		la	$a1,	inputBuffer
+		li	$a2,	100
+		syscall
+
+exit_it:	li	$v0,	16
+		move	$a0,	$s6
+		syscall
+
+buffer_Loop:	
