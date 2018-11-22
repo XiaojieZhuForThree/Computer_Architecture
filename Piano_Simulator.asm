76d14ae02c14150de508bfb09440f1145f4bfea2
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2018-11-21 18:14:46 -0600

    Update Piano_Simulator.asm

--- Piano_Simulator.asm
+++ Piano_Simulator.asm
@@ -1,4 +1,4 @@
-	.data
+.data
 input:		.asciiz		"Please select the instrument you want to play"
 output1:	.asciiz		"You have selected to play the "
 output2:	.asciiz		"Please connect the Keyboard Simulator"
@@ -12,24 +12,114 @@ key7:		.byte 		'7'
 key8:		.byte 		'8'
 key9:		.byte		'9'
 key10:		.byte 		'0'
-
+key11:		.byte		'a'
+key12:		.byte		'b'
 
 	.text
-IO: 	li 	$t0, 	0xffff0000
-	beqz	$t0,	IO
-	lw 	$t1, 	0xffff0004
+	lui	$t0,	0xffff
+IO: 	lw	$t1,	0($t0)
+	andi	$t1,	$t1,	0x0001
+	beq	$t1,	$zero,	IO
+	lbu  	$t1, 	4($t0)
 
 	
 key:	lbu	$t2,	key1
 	beq	$t1,	$t2, 	play1
-	addi	$t0,	$t0,	1
-	b 	IO
-	
+	lbu	$t2,	key2
+	beq	$t1,	$t2, 	play2
+	lbu	$t2,	key3
+	beq	$t1,	$t2, 	play3
+	lbu	$t2,	key4
+	beq	$t1,	$t2, 	play4
+	lbu	$t2,	key5
+	beq	$t1,	$t2, 	play5
+	lbu	$t2,	key6
+	beq	$t1,	$t2, 	play6
+	lbu	$t2,	key7
+	beq	$t1,	$t2, 	play7
+	lbu	$t2,	key8
+	beq	$t1,	$t2, 	play8
+	lbu	$t2,	key9
+	beq	$t1,	$t2, 	play9
+	lbu	$t2,	key10
+	beq	$t1,	$t2, 	play10
+		
 play1:	li	$v0,	31
+	li	$a0, 	61
+	li	$a1,	1000
+	li	$a2,	0
+	li	$a3,	127
+	syscall
+	j 	IO
+	
+play2:	li	$v0,	31
+	li	$a0, 	62
+	li	$a1,	1000
+	li	$a2,	0
+	li	$a3,	127
+	syscall
+	j 	IO
+
+play3:	li	$v0,	31
+	li	$a0, 	63
+	li	$a1,	1000
+	li	$a2,	0
+	li	$a3,	127
+	syscall
+	j 	IO
+
+play4:	li	$v0,	31
+	li	$a0, 	64
+	li	$a1,	1000
+	li	$a2,	0
+	li	$a3,	127
+	syscall
+	j 	IO
+
+play5:	li	$v0,	31
+	li	$a0, 	65
+	li	$a1,	1000
+	li	$a2,	0
+	li	$a3,	127
+	syscall
+	j 	IO
+
+play6:	li	$v0,	31
+	li	$a0, 	66
+	li	$a1,	1000
+	li	$a2,	0
+	li	$a3,	127
+	syscall
+	j 	IO
+
+play7:	li	$v0,	31
+	li	$a0, 	67
+	li	$a1,	1000
+	li	$a2,	0
+	li	$a3,	127
+	syscall
+	j 	IO
+
+play8:	li	$v0,	31
+	li	$a0, 	68
+	li	$a1,	1000
+	li	$a2,	0
+	li	$a3,	127
+	syscall
+	j 	IO
+	
+play9:	li	$v0,	31
+	li	$a0, 	69
+	li	$a1,	1000
+	li	$a2,	0
+	li	$a3,	127
+	syscall
+	j 	IO
+
+play10:	li	$v0,	31
 	li	$a0, 	70
 	li	$a1,	1000
 	li	$a2,	0
 	li	$a3,	127
 	syscall
-	addi	$t0,	$t0,	1
-	b 	IO
+	j 	IO
